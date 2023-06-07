440293fb970f63c50651e76f4853dc8e83d51c30
sukanya <sukanyasirigiri@gmail.com>
2023-06-07 10:05:27 -0400

    sub

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_autoscaling_group" "asg" {
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
-  vpc_zone_identifier = var.subnet_ids
+  vpc_zone_identifier = var.subnet_id
 
   
   
